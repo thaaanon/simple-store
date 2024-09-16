04a6cad215c4799d764d99bda07116ee7dc73914
RefactorDebug <refactoring.usage959@passinbox.com>
2024-09-16 15:22:36 +0530

    Seeded products with Faker

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,10 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+require 'faker'
+
+676.times do
+  Product.create(
+    title: Faker::Commerce.product_name,
+    description: Faker::Lorem.sentence,
+    price: Faker::Commerce.price,
+    stock_quantity: Faker::Number.between(from: 1, to: 100)
+  )
+end
